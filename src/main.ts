7e92501c06947985fb6e0a9d54affac857da7055
smile1986777 <smile777usa@gmail.com>
2025-08-03 23:04:37 +0600

    v0.2

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'https://an-vadim-an-mini-app-clicker-frontend-ab25.twc1.net/',
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
